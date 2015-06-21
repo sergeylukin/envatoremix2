3a0518d3554044eb9bf802ce4f6353072cded1bb
Sergey Lukin <contact@sergeylukin.com>
2015-06-21 03:13:58 +0300

    Update 2015-06-21T00:13:54.943Z

--- README.md
+++
@@ -1,4 +0,0 @@
-# Envato Remix 2 contest
-
-Gulp powered project crafted with love on Macbook Pro in Israel in 2015 specially for
-[Envato Remix 2 contest](http://contests.envato.com/remix-02/index.html).
--- gulpfiles/tasks/deploy.js
+++
@@ -1,7 +0,0 @@
-var gulp    = require('gulp');
-var ghPages = require('gulp-gh-pages');
-
-gulp.task('deploy', ['build:production'], function() {
-  return gulp.src('./build/production/**/*')
-    .pipe(ghPages());
-});
